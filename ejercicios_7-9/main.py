cd74cd202d68d1949107ef32beddeb3956bb1996
bretmer <condorihuamanbrink05@gmail.com>
2024-05-21 14:10:57 -0500

    solucione mi carpeta de los ejercicios 7-9

--- ejercicios_7-9/main.py
+++ ejercicios_7-9/main.py
@@ -9,11 +9,26 @@ for n in range(0,len(frase)):
 print(f"la letra {letra} aparece {contador_letra} veces en la frase")
 #**Ejercicio Ocho**
 ##Escribir un programa que pida al usuario una palabra y luego muestre por pantalla la primera letra la letra que se encuentra en medio y la ultima letra separadas por comas(,).
-letras:str=""
-palabra:str=input("ingrese un palabra: ")
-for n in palabra:
-    letras+=palabra[-1]
-    print(letras)
-    
+primera_letra:str=""
+letra_medio:str=""
+ultima_letra:str=""
+for n in range(1):
+    palabra:str=input("ingrese una palabra: ")
+    primera_letra+=palabra[0]
+    letra_medio+=palabra[len(palabra) // 2]
+    ultima_letra+=palabra[-1]
+letras=primera_letra+letra_medio+ultima_letra
+print(letras)  
+
 #**Ejercicio Nueve**
 ##Escriba un programa que pregunte cuántos números se van a introducir, luego pida esos números, y muestre un mensaje cada vez que un número no sea mayor que el anterior.
+valores = int(input("¿Cuántos valores vas a introducir? "))
+if valores > 0:
+    numero_anterior = int(input("Introduce un número: "))
+    for i in range(1, valores):
+        numero_actual = int(input("Introduce el siguiente número: "))
+        if numero_actual <= numero_anterior:
+            print("El número introducido no es mayor que el anterior.") 
+        numero_actual = numero_anterior       
+else:
+    print("¡IMPOSIBLE!")
